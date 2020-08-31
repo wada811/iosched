76dddac006a19471d69ddc423e594e9c9e4f00d5
wada811 <at.wada811@gmail.com>
2020-08-31 10:42:33 +0900

    Add CoroutinesDependencyModule

---
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/di/CoroutinesDependencyModule.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.iosched.shared.di
+
+import com.wada811.dependencyproperty.DependencyModule
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+open class CoroutinesDependencyModule : DependencyModule {
+    open val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+    open val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    open val mainDispatcher: CoroutineDispatcher = Dispatchers.Main
+    open val mainImmediateDispatcher: CoroutineDispatcher = Dispatchers.Main.immediate
+}
