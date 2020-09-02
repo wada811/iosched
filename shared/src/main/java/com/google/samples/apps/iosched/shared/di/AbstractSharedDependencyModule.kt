79a7fdd588647feb9efcd6504fb7011beb35a978
wada811 <at.wada811@gmail.com>
2020-09-02 22:29:46 +0900

    Add observeUserAuthStateUseCase to AbstractSharedDependencyModule

--- shared/src/main/java/com/google/samples/apps/iosched/shared/di/AbstractSharedDependencyModule.kt
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/di/AbstractSharedDependencyModule.kt
@@ -55,6 +55,7 @@ import com.google.samples.apps.iosched.shared.data.userevent.DefaultSessionAndUs
 import com.google.samples.apps.iosched.shared.data.userevent.FirestoreUserEventDataSource
 import com.google.samples.apps.iosched.shared.data.userevent.SessionAndUserEventRepository
 import com.google.samples.apps.iosched.shared.data.userevent.UserEventDataSource
+import com.google.samples.apps.iosched.shared.domain.auth.ObserveUserAuthStateUseCase
 import com.google.samples.apps.iosched.shared.domain.search.FtsMatchStrategy
 import com.google.samples.apps.iosched.shared.domain.search.SessionTextMatchStrategy
 import com.google.samples.apps.iosched.shared.domain.search.SimpleMatchStrategy
@@ -150,4 +151,13 @@ abstract class AbstractSharedDependencyModule(
     }
     abstract val authStateUserDataSource: AuthStateUserDataSource
     abstract val authIdDataSource: AuthIdDataSource
+    val observeUserAuthStateUseCase: ObserveUserAuthStateUseCase by lazy {
+        ObserveUserAuthStateUseCase(
+            registeredUserDataSource,
+            authStateUserDataSource,
+            topicSubscriber,
+            applicationScope,
+            coroutinesDependencyModule.ioDispatcher
+        )
+    }
 }
