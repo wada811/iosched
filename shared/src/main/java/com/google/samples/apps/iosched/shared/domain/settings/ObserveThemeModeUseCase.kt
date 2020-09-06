56b0287a42bbefd649afc4be639366a9ce8ab286
wada811 <at.wada811@gmail.com>
2020-09-07 00:05:27 +0900

    Remove annotation in ObserveThemeModeUseCase

--- shared/src/main/java/com/google/samples/apps/iosched/shared/domain/settings/ObserveThemeModeUseCase.kt
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/domain/settings/ObserveThemeModeUseCase.kt
@@ -21,17 +21,15 @@ import com.google.samples.apps.iosched.model.Theme
 import com.google.samples.apps.iosched.model.Theme.BATTERY_SAVER
 import com.google.samples.apps.iosched.model.themeFromStorageKey
 import com.google.samples.apps.iosched.shared.data.prefs.PreferenceStorage
-import com.google.samples.apps.iosched.shared.di.DefaultDispatcher
 import com.google.samples.apps.iosched.shared.domain.FlowUseCase
 import com.google.samples.apps.iosched.shared.result.Result
-import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-open class ObserveThemeModeUseCase @Inject constructor(
+open class ObserveThemeModeUseCase(
     private val preferenceStorage: PreferenceStorage,
-    @DefaultDispatcher dispatcher: CoroutineDispatcher
+    dispatcher: CoroutineDispatcher
 ) : FlowUseCase<Unit, Theme>(dispatcher) {
     override fun execute(parameters: Unit): Flow<Result<Theme>> {
         return preferenceStorage.observableSelectedTheme.map {
