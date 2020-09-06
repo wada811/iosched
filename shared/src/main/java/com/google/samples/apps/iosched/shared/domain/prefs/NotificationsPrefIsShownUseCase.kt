92c3b87fe060605b0ff7f515bf23c15c5bf453e5
wada811 <at.wada811@gmail.com>
2020-09-07 00:35:28 +0900

    Remove annotation in NotificationsPrefIsShownUseCase

--- shared/src/main/java/com/google/samples/apps/iosched/shared/domain/prefs/NotificationsPrefIsShownUseCase.kt
+++ shared/src/main/java/com/google/samples/apps/iosched/shared/domain/prefs/NotificationsPrefIsShownUseCase.kt
@@ -17,17 +17,15 @@
 package com.google.samples.apps.iosched.shared.domain.prefs
 
 import com.google.samples.apps.iosched.shared.data.prefs.PreferenceStorage
-import com.google.samples.apps.iosched.shared.di.IoDispatcher
 import com.google.samples.apps.iosched.shared.domain.UseCase
-import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 
 /**
  * Returns whether the notifications preference has been shown to the user.
  */
-open class NotificationsPrefIsShownUseCase @Inject constructor(
+open class NotificationsPrefIsShownUseCase(
     private val preferenceStorage: PreferenceStorage,
-    @IoDispatcher dispatcher: CoroutineDispatcher
+    dispatcher: CoroutineDispatcher
 ) : UseCase<Unit, Boolean>(dispatcher) {
     override suspend fun execute(parameters: Unit): Boolean =
         preferenceStorage.notificationsPreferenceShown
